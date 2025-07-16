16c83e3c42325a3e7f4e570a8e8a20058454052d
Adedapo Stephen <stevvodapsy@gmail.com>
2025-07-16 15:09:04 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,15 +3,6 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBpg8X1lVqXiPxsHWAFpA6-0zOmd9cQF6I",
-  authDomain: "dwello-app.firebaseapp.com",
-  projectId: "dwello-app",
-  storageBucket: "dwello-app.firebasestorage.app",
-  messagingSenderId: "952713433386",
-  appId: "1:952713433386:web:ab90215bcb991c039d4e53",
-  measurementId: "G-0C9YNH4HVX"
-};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
